9478c33b5928e76ffd971465b1de5f7d0316bc5c
santoldev <tikolaso043@gmail.com>
2025-07-27 16:17:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // host: true, // Listen on all interfaces
+    allowedHosts: true, // Allow all domains and subdomains
+    // port: 5173, // Optional: specify port
+    // cors: true, // Optional: enable CORS for API/assets
+  },
 })
